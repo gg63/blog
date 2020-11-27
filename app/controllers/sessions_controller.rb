0b8c99ad9a34e385afe2cdb7ae476e655953b04d
gg <wakadanna0603@gmail.com>
2020-11-27 18:23:24 +0000

    login&logout画面完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,17 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenicate(params[:password])
-      ssesion[:user_id] = user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to user_path, notice: "認証確認ログインしました"
     else
       flash.now[:alert] = "emailまたはpasswordが違います"
       render :new
     end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "ログアウトしました"
   end
 end
