b2a1dbcc84ba9e800021b994cee0b4169df4df61
gg <wakadanna0603@gmail.com>
2020-11-28 19:25:19 +0000

    制限機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user
+    
+    def login?
+        if current_user.nil?
+            redirect_to login_path, alert: "ログインする必要があります"
+        end
+    end
+    
+    def already_login?
+        unless current_user.nil?
+            redirect_to user_path, notice: "あなたは既にログイン済みです"
+        end
+    end
+    
+    def current_user
+        if session[:user_id]
+            current_user ||= User.find(session[:user_id])
+        end 
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  before_action :already_login?, only: [:new, :create]
+  before_action :login?, only: :show
+  
   def new
     @user = User.new
   end
-  
+
   def create
     user = User.new(user_params)
     if user.save
@@ -15,7 +18,7 @@ class UsersController < ApplicationController
 
   def show
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
